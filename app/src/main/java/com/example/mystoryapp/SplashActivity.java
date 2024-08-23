b11d85f671e0c138a24b09725d2777645e4b4d82
OdeliaTamar <odeliash@edu.hac.ac.il>
2024-08-23 13:03:35 +0300

    the logo in the beginning of the app is working

---
+++ app/src/main/java/com/example/mystoryapp/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.mystoryapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.mystoryapp.MainActivity;
+import com.example.mystoryapp.R;
+import pl.droidsonroids.gif.GifImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        GifImageView gifImageView = findViewById(R.id.gifImageView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4900);  // 5000 מילישניות = 5 שניות
+    }
+}
